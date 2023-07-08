bab5a5f6d97ee15d3c11ae6a1b834f01a20d4f2b
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-07 22:01:52 -0400

    checkpoint

---
+++ app/src/main/java/com/example/recycleviewm5i5/AdapterWords.java
@@ -0,0 +1,48 @@
+package com.example.recycleviewm5i5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recycleviewm5i5.databinding.ItemBinding;
+
+import java.util.List;
+
+public class AdapterWords extends RecyclerView.Adapter<AdapterWords.ViewHolder> {
+
+    private List<String> words;
+    @NonNull
+    @Override
+    public AdapterWords.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ItemBinding binding = ItemBinding.inflate(LayoutInflater.from(parent.getContext()));
+
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterWords.ViewHolder holder, int position) {
+        String item = words.get(position);
+        holder.mostrarInformacion(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return words.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemBinding itembinding;
+        public ViewHolder(@NonNull ItemBinding binding) {
+            super(binding.getRoot());
+            itembinding = binding;
+        }
+
+        public void mostrarInformacion(String words){
+            itembinding.textView.setText(words);
+        }
+    }
+}
